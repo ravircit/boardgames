886a34de011e4de0592bb2c23de820e8b04d200c
Ravi <ravircit@gmail.com>
2017-11-07 15:28:21 +0530

    1st Commit

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Testing Git Commit")
+    return render(request,"main/home.html")
 
 def index(request):
     return HttpResponse("Testing Git Commit")
