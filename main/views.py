be0ee4691353ab9b4069ae30c934dc1a2e23c174
Ravi <ravircit@gmail.com>
2017-11-07 14:59:10 +0530

    1st Commit

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("hiiiiiiiiiiii")
+    return HttpResponse("Hello there")
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
